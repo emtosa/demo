06901cae76532662272e343d07e6b8c10cbdee7d
Tosa Ojiru <2237968+emtosa@users.noreply.github.com>
2021-03-29 06:52:36 -0700

    Demo factory pattern using coffee example

---
+++ DesignPatternsDemo/README.md
@@ -0,0 +1,8 @@
+# Introduction
+When implementing this pattern, the focus is on building the or the product.
+
+# Scenario
+Carlos needs coffee but does not know how to make it, however, a John who is a barista knows how to make coffee.
+
+# Reference
+https://www.youtube.com/watch?v=ejfbbAlDUvE&list=PLan3SCnsISTSzm1HV9Zuc_IyjpJ6DAId-&index=2
---
+++ DesignPatternsDemo/Starbucks.App/Capuccino.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starbucks.App
+{
+    public class Capuccino : IDrink
+    {
+        public Capuccino(string[] ingredients)
+        {
+        }
+
+        public void Making()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Price()
+        {
+            return 5;
+        }
+
+        public int Size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
