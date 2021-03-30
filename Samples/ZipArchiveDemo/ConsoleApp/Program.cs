a1af00f5adfad6bc59d28f6aa204ff5eb049f46f
Tosa Ojiru <2237968+emtosa@users.noreply.github.com>
2021-03-29 20:51:23 -0700

    Create zipped archive

---
+++ Samples/ZipArchiveDemo/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream zipToOpen = new FileStream(@"C:\emtosa\demo\Samples\ZipArchiveDemo\ConsoleApp\release.zip", FileMode.OpenOrCreate))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    ZipArchiveEntry readmeEntry = archive.CreateEntry("Readme.txt");
+                    using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
+                    {
+                        writer.WriteLine("Information about this package");
+                        writer.WriteLine("==============================");
+                    }
+                }
+            }
+        }
+    }
+}
