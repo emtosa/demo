b1f4c57b598c88848bf7b8d4ab56b4c8bb649fbb
Tosa Ojiru <tosao@microsoft.com>
2021-04-17 16:58:28 -0700

    Update launch settings to enable debugging on vscode

--- Samples/csharpdemo/gradebook/src/GradeBook/Program.cs
+++ Samples/csharpdemo/gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Tosa!");
+            Console.WriteLine($"Hello {args[0]}!");
         }
     }
 }
