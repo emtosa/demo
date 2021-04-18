36eec95f6dc5f0f17126501dd15296379b079ba7
Tosa Ojiru <tosao@microsoft.com>
2021-04-18 16:26:05 -0700

    experimenting with delegates

--- Samples/csharpdemo/gradebook/src/GradeBook/Book.cs
+++ Samples/csharpdemo/gradebook/src/GradeBook/Book.cs
@@ -28,13 +28,11 @@ namespace GradeBook
 
         public string Name
         {
-            set {
-                name = value;
-            }
-            get {
-                return name;
-            }
+            set;
+            get;
         }
+
+        readonly string category = "science";
 
         public Book(string name)
         {
@@ -42,9 +40,36 @@ namespace GradeBook
             Name = name;
         }
 
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid grade {nameof(grade)}");
+            }
         }
 
         public Statistics GetStatistics()
@@ -60,6 +85,27 @@ namespace GradeBook
                 result.Average += grade;
             }
             result.Average = result.Average / grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
     }
