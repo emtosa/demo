c1e106cdd7d68d20dcdad1cbfd69f1a8be9272bc
Tosa Ojiru <tosao@microsoft.com>
2021-04-18 12:28:13 -0700

    experimenting with references

--- Samples/csharpdemo/gradebook/test/GradeBook.Tests/TypeTests.cs
+++ Samples/csharpdemo/gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,47 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CSharpCanPassByRefUsingOut()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetNameByReferenceUsingOut(out book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+        private void GetBookSetNameByReferenceUsingOut(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetNameByReference(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetNameByReference(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
         public void CanSetNameFromReference()
         {
             var book1 = GetBook("Book 1");
